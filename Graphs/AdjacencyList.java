379cc4cc79ec0a70228cdcdccb6afe330b8823c1
Bharat Gupta <bharatgupta@Bharats-MacBook-Air-2.local>
2018-10-22 01:52:46 +0530

    add AdjacencyList

--- Graphs/AdjacencyList.java
+++ Graphs/AdjacencyList.java
@@ -2,11 +2,39 @@ import java.util.LinkedList;
 
 //undirected graph
 class AdjacencyList {
-	public static void main(String args[]) {
-		LinkedList<Integer>[] graph = new LinkedList[4];
-		for(int i = 0; i < 4; i++) {
-			graph[i] = new LinkedList<Integer>();
+	
+	private static class Graph {
+		private int V;
+		private LinkedList<Integer>[] g;
+
+		private Graph(int V) {
+			this.V = V;
+			g = new LinkedList[V];
+			for(int i = 0; i < V; i++) {
+				g[i] = new LinkedList<Integer>();
+			}
 		}
+	}
+
+	public static void add(Graph graph, int a, int b) {
+		graph.g[a].add(b);
+		graph.g[b].add(a);
+	}
+
+	public static void print(Graph graph) {
+		for(int i = 0; i < 4; i++) {
+			for(Integer val : graph.g[i]) {
+				System.out.print(val);
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public static void main(String args[]) {
+
+		//5 = no. of verteces
+		Graph graph = new Graph(5);
+		
 		add(graph, 0, 1);
 		add(graph, 0, 3);
 		add(graph, 1, 2);
@@ -15,19 +43,7 @@ class AdjacencyList {
 		print(graph);
 	}
 
-	public static void add(LinkedList<Integer>[] graph, int a, int b) {
-		graph[a].add(b);
-		graph[b].add(a);
-	}
-
-	public static void print(LinkedList<Integer>[] graph) {
-		for(int i = 0; i < 4; i++) {
-			for(Integer val : graph[i]) {
-				System.out.print(val);
-			}
-			System.out.print("\n");
-		}
-	}
+	
 }
 
 /*
